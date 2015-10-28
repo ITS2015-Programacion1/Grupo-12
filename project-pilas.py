11e4bec0886d4050b66167e805bcf52b2fc03aee
Tobias Palacios Isola <tobiasjpalacios@gmail.com>
2015-10-28 11:00:53 -0300

    Update project-pilas.py

--- project-pilas.py
+++ project-pilas.py
@@ -1,1 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import pilasengine 
+pilas = pilasengine.iniciar()
+#agregando el personaje 
+runner = pilas.actores.Shaolin()
+runner.x = -100
+runner.y = -100
+runner.escala = 0.2
+runner.aprender("LimitadoABordesDePantalla")
 
