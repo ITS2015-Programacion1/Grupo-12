9a88fc02f680e3fd616f21323df45570cb2498a3
Tobias Palacios Isola <tobiasjpalacios@gmail.com>
2015-10-28 15:57:01 -0300

    correcion de valores de orientacion

--- project-pilas.py
+++ project-pilas.py
@@ -4,8 +4,8 @@ import pilasengine
 pilas = pilasengine.iniciar()
 #agregando el personaje 
 runner = pilas.actores.Shaolin()
-runner.x = -100
-runner.y = -100
+runner.x = -500
+runner.y = -500
 runner.escala = 0.2
 runner.aprender("LimitadoABordesDePantalla")
 
